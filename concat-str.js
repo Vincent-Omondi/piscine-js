1b8ce8b366760205c7241f2cfaf97ca5944e1bb4
vinomondi <vincentomondi1002@gmail.com>
2024-08-26 14:31:50 +0300

    fix: concatstr for numbers

--- concat-str.js
+++ concat-str.js
@@ -1,5 +1,6 @@
 function concatStr(str1, str2){
-    return  str1 + str2;
+    return  String(str1) + String(str2);
 }
 
 // console.log(concatStr("Hello, ", "World!")); 
+console.log(concatStr(1,2))
