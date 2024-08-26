aaae67773b3a228b09217cf0b6b2442f981fa8f3
vinomondi <vincentomondi1002@gmail.com>
2024-08-26 17:19:46 +0300

    Create a min and max functions to return min and max numbers

---
+++ min-max.js
@@ -0,0 +1,18 @@
+function max(num1, num2){
+    if (num1 > num2){
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
+function min(num1, num2){
+    if (num1 < num2){
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
+// console.log(max(5, 8))
+// console.log(min(5, 8))
