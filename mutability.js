3e274ee912609426a8b5403da354ebd5ecaa3988
vinomondi <vincentomondi1002@gmail.com>
2024-08-26 12:39:47 +0300

    Create copies of person object, commented the unwanted parts

--- mutability.js
+++ mutability.js
@@ -1,8 +1,8 @@
-const person = {
-    name: 'Rick',
-    age: 77,
-    country: 'US',
-};  
+// const person = {
+//     name: 'Rick',
+//     age: 77,
+//     country: 'US',
+// };  
 
 let clone1 = {...person};
 let clone2 = {...person};
@@ -11,7 +11,7 @@ let samePerson = person;
 person.age = 78;
 person.country = 'FR';
 
-console.log(clone1.age)
-console.log(clone2.age)
-console.log(person.age)
-console.log(samePerson.country)
+// console.log(clone1.age)
+// console.log(clone2.age)
+// console.log(person.age)
+// console.log(samePerson.country)
