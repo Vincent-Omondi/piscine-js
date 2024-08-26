59e8e3b844a75a520b3d6768791335f6a8bb3d44
vinomondi <vincentomondi1002@gmail.com>
2024-08-26 12:53:47 +0300

    Add arithmetic functions (more, less, add, sub)

---
+++ more-or-less.js
@@ -0,0 +1,20 @@
+function more(num){
+    return  num+1;
+}
+
+function less(num){
+    return num-1;
+}
+
+function add(a, b){
+    return a+b;
+}
+
+function sub(a,b){
+    return a-b;
+}
+
+// console.log(more(2))
+// console.log(less(3))
+// console.log(add(2,3))
+// console.log(sub(2,3))
