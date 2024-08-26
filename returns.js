7128c17616626050fb212fcc0143c015032d59b3
vinomondi <vincentomondi1002@gmail.com>
2024-08-26 13:08:50 +0300

    feat: Add id and getLength utility functions

---
+++ returns.js
@@ -0,0 +1,11 @@
+function id(arg){
+    return arg;
+}
+
+function getLength(input){
+    return input.length;
+}
+
+// console.log(id("Hello")); 
+// console.log(getLength("Hello"));
+// console.log(getLength([1, 2, 3, 4]));
