9764b405df284c599a5baa94c189ac53180a5b71
vinomondi <vincentomondi1002@gmail.com>
2024-08-27 08:37:44 +0300

    create a function that returns 1 for positive number, -1 for negative number and 0 if number is zero

---
+++ sign.js
@@ -0,0 +1,13 @@
+function sign(num){
+    if (num > 0){
+        return 1;
+    } else if (num  < 0){
+        return  -1;
+    } else {
+        return 0;
+    }
+}
+
+// console.log(sign(7));
+// console.log(sign(-5));
+// console.log(sign(0));
