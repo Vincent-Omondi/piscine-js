8e11cf11c1c6f7c1bb9d56379517b2e2544c7826
vinomondi <vincentomondi1002@gmail.com>
2024-08-29 14:55:50 +0300

    refactor: comment unwanted codes

--- cut-corners.js
+++ cut-corners.js
@@ -47,8 +47,8 @@ function getFloor(x) {
     return floor;
 }
 
-const nums = [3.7, -3.7, 3.1, -3.1];
-console.log(nums.map(round));
-console.log(nums.map(floor));
-console.log(nums.map(trunc));
-console.log(nums.map(ceil));
+// const nums = [3.7, -3.7, 3.1, -3.1];
+// console.log(nums.map(round));
+// console.log(nums.map(floor));
+// console.log(nums.map(trunc));
+// console.log(nums.map(ceil));
