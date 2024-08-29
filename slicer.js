606907402f35c3796224f72f946f785f657d911f
vinomondi <vincentomondi1002@gmail.com>
2024-08-29 09:53:05 +0300

    fix slice function to handle negative indexing for both slices and strings

--- slicer.js
+++ slicer.js
@@ -1,4 +1,6 @@
 function slice(input, start=0, end = input.length -1){
+    if (start < 0) start = input.length + start;
+    if (end < 0) end = input.length + end;
     if (Array.isArray(input)){
         let result = [];
         for (let i=start; i <= end;  i++){
@@ -6,20 +8,18 @@ function slice(input, start=0, end = input.length -1){
         }
         return result;
     } else {
-        if (start < 0) start = input.length + start;
-        if (end < 0) end = input.length + end;
         let result = '';
         for (let i=start; i <= end;  i++){
             result += input[i];
         }
         return result;
-    }
-
-    
+    }    
 }
 
-// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
-// const str = "Hello World!";
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const str = "Hello World!";
 
-// console.log(slice(fruits,2,4));
-// console.log(slice(str,0,5));
+console.log(slice(fruits,2,4));
+console.log(slice(str,0,5));
+console.log(slice(fruits,-2));
+console.log(slice(str,-2));
