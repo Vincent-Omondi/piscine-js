6b8167e99f6f34466ac9a0ed2909154594289c5c
vinomondi <vincentomondi1002@gmail.com>
2024-08-29 09:42:59 +0300

    Add slice function to mimic Array.slice() and String.slice() behavior

---
+++ slicer.js
@@ -0,0 +1,23 @@
+function slice(input, start=0, end = input.length -1){
+    if (Array.isArray(input)){
+        let result = [];
+        for (let i=start; i <= end;  i++){
+            result.push(input[i]);
+        }
+        return result;
+    } else {
+        let result = '';
+        for (let i=start; i <= end;  i++){
+            result += input[i];
+        }
+        return result;
+    }
+
+    
+}
+
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const str = "Hello World!"
+
+// console.log(slice(fruits,2,4))
+// console.log(slice(str,0,5))
