5eb8e403a06e9e7005b6f8bbc54e0d2ed0b8d370
vinomondi <vincentomondi1002@gmail.com>
2024-08-29 12:02:37 +0300

    fix: adjust the index

--- slicer.js
+++ slicer.js
@@ -3,23 +3,24 @@ function slice(input, start=0, end = input.length -1){
     if (end < 0) end = input.length + end;
     if (Array.isArray(input)){
         let result = [];
-        for (let i=start; i <= end;  i++){
+        for (let i=start; i < end;  i++){
             result.push(input[i]);
         }
         return result;
     } else {
         let result = '';
-        for (let i=start; i <= end;  i++){
+        for (let i=start; i < end;  i++){
             result += input[i];
         }
         return result;
     }    
 }
 
-const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
-const str = "Hello World!";
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const str = "Hello World!";
 
-console.log(slice(fruits,2,4));
-console.log(slice(str,0,5));
-console.log(slice(fruits,-2));
-console.log(slice(str,-2));
+// console.log(slice(fruits,2,4));
+// console.log(slice(str,0,5));
+// console.log(slice(fruits,-2));
+// console.log(slice(str,-2));
+console.log(slice('abcdef', 0, 2)) //() => slice('abcdef', 0, 2) === 'ab'
