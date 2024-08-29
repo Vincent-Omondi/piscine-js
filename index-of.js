36e240633d281565a08bf2ddfcf275c09aa5802d
vinomondi <vincentomondi1002@gmail.com>
2024-08-29 08:54:33 +0300

    Implemented custom array search functions: indexOf, lastIndexOf, and includes

---
+++ index-of.js
@@ -0,0 +1,27 @@
+function indexOf(arr, value,  fromIndex=0) {
+    for (let i=fromIndex; i < arr.length; i++){
+        if (arr[i] === value){
+            return i;
+        }  
+    }
+    return -1;
+}
+
+function lastIndefOf(arr, value){
+    for (let i = arr.length -1; i >= 0; i--){
+        if (arr[i] === value){
+            return i;
+        }
+    }
+    return -1;
+}
+
+function includes(arr, value){
+    return indexOf(arr, value) !== -1;
+}
+
+const arr = [1, 2, 3, 4, 2,0,9, 5];
+
+console.log(indexOf(arr, 2, fromIndex = 0))
+console.log(lastIndefOf(arr, 2))
+console.log(includes(arr, 9))
