023f05c5cf4a7ef7f5d8c4651de21452c51125d6
vinomondi <vincentomondi1002@gmail.com>
2024-08-29 17:29:33 +0300

    Implement functions that act as split and join

---
+++ unbreakable.js
@@ -0,0 +1,39 @@
+function split(str, del = ''){
+    let result = [];
+    let word = '';
+    for (let i = 0; i <= str.length - 1; i++){
+        
+        if (str[i] !== del){
+            word += str[i]
+        } else {
+            result.push(word);
+            word = '';
+        }
+    }
+    result.push(word);
+    return result
+}
+
+function join(str, del =','){
+    let result = '';
+
+    for (let i = 0; i >= str.length; i++){
+        result += arr[i]
+        if (i < arr.length -1){
+            result += del;
+        }
+    }
+    return result;
+}
+
+
+
+// const str = 'The quick brown fox jumps over the lazy dog.';
+
+
+// const words = str.split(' ');
+// console.log(words);
+
+// const elements = ['Fire', 'Air', 'Water'];
+
+// console.log(elements.join());
