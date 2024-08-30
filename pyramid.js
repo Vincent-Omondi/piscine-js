2e381aa32aab1d8bc7591af245201e9bb5c1a18a
vinomondi <vincentomondi1002@gmail.com>
2024-08-30 15:10:56 +0300

    Add the `pyramid` function that constructs a centered pyramid using a specified character and depth.

---
+++ pyramid.js
@@ -0,0 +1,29 @@
+function pyramid(character, depth) {
+    let result = '';
+
+    for (let i = 1; i <= depth; i++) {
+        let line = '';
+
+        const numSpaces = depth - i;
+        const numChars = 2 * i - 1;
+
+        for (let j = 0; j < numSpaces; j++) {
+            line += ' ';
+        }
+
+        for (let k = 0; k < numChars; k++) {
+            line += character;
+        }
+
+        result += line;
+
+        if (i < depth) {
+            result += '\n';
+        }
+    }
+
+    return result;
+}
+
+// console.log(pyramid('*', 5));
+
