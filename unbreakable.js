ed352fec770c9d1c933a9aaaa3ae393fb3197ec5
Vincent-Omondi <vincentomondi251@gmail.com>
2024-08-30 08:20:12 +0300

    fix: array indexing

--- unbreakable.js
+++ unbreakable.js
@@ -1,23 +1,26 @@
 function split(str, del = ''){
+    if (del === '') return [str];
+
     let result = [];
     let word = '';
-    for (let i = 0; i <= str.length - 1; i++){
+    for (let i = 0; i < str.length; i++){
         
-        if (str[i] !== del){
+        if (str.slice(i,i + del.length) !== del){
             word += str[i]
         } else {
             result.push(word);
             word = '';
+            i += del.length -1
         }
     }
     result.push(word);
     return result
 }
 
-function join(str, del =','){
+function join(arr, del =','){
     let result = '';
 
-    for (let i = 0; i >= str.length; i++){
+    for (let i = 0; i < arr.length; i++){
         result += arr[i]
         if (i < arr.length -1){
             result += del;
@@ -28,12 +31,9 @@ function join(str, del =','){
 
 
 
-// const str = 'The quick brown fox jumps over the lazy dog.';
+// const str = 'ggg - ddd - b';
+// const splitResult = split(str, ' - ');
+// console.log(splitResult); // ['ggg', 'ddd', 'b']
 
-
-// const words = str.split(' ');
-// console.log(words);
-
-// const elements = ['Fire', 'Air', 'Water'];
-
-// console.log(elements.join());
+// const joinResult = join(splitResult, ' - ');
+// console.log(joinResult); // 'ggg - ddd - b'
