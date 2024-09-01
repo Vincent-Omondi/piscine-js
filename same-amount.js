dd0bba12112e7802bb668828b4e3fa2040e1b470
vinomondi <vincentomondi1002@gmail.com>
2024-09-01 09:48:12 +0300

    fix: Handle cases with no matches in sameAmount function

--- same-amount.js
+++ same-amount.js
@@ -1,10 +1,17 @@
-function sameAmount(str, reg1, reg2){
-    const matches1 = str.match(reg1) || [];
+function sameAmount(str, reg1, reg2) {
+    function countMatches(str, regex) {
+        const globalRegex = new RegExp(regex, regex.flags + (regex.global ? '' : 'g'));
+        return (str.match(globalRegex) || []).length;
+    }
 
-    const matches2 = str.match(reg2) || [];
+    const count1 = countMatches(str, reg1);
+    const count2 = countMatches(str, reg2);
 
-    return matches1.length === matches2.length;
+    return count1 === count2;
 }
 
 // console.log(sameAmount('ababab', /a/g, /b/g)); 
 // console.log(sameAmount('abacaba', /a/g, /b/g));
+
+// const data = "some string without qqqqqqq";
+// console.log(sameAmount(data, /q /g, /qqqqqqq/g)); 
