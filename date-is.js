3994e290c75372f4af800258cf782fdefa29370d
vinomondi <vincentomondi1002@gmail.com>
2024-09-03 09:00:57 +0300

    fix: passing new date as a string

--- date-is.js
+++ date-is.js
@@ -1,23 +1,27 @@
 function isValid(dateStr){
-    return !isNaN(new Date(dateStr).getTime());
+    return dateStr instanceof Date && !isNaN(dateStr);
 }
 
 function isAfter(currentDate, inputDate){
-   if (!isValid(currentDate) || !isValid(inputDate)) return "Invalid Dates";
-   return new Date (currentDate) > new Date (inputDate);
+   if (!isValid(currentDate) || !isValid(inputDate)) return false;
+   return (currentDate) > (inputDate);
 }
 
 function isBefore(currentDate, inputDate){
-    if (!isValid(currentDate) || !isValid(inputDate)) return " Invalid dates";
-    return new Date(currentDate) < new Date(inputDate);
+    if (!isValid(currentDate) || !isValid(inputDate)) return false;
+    return (currentDate) < (inputDate);
+}
+
+function isFuture(date){
+    return isValid(date) && (date) > new Date();
 }
 
 function isPast(date){
-    return isValid(date) && new Date(date) < new Date();
+    return isValid(date) && (date) < new Date();
 }
 
 
-// console.log(isValid("10/05/2019"));
+// console.log(isValid('2013/01/01'));
 // console.log(isAfter("9/9/2024", "6/7/2024"))
 // console.log(isBefore("9/9/2024", "6/7/2024"))
 // console.log(isPast("09/02/2024"))
