0d82d35185ea85816a3dab6563ef94b5b47cac4d
vinomondi <vincentomondi1002@gmail.com>
2024-09-03 12:54:40 +0300

    fix: case when year is 1953 and below

--- get-some-time.js
+++ get-some-time.js
@@ -4,10 +4,17 @@ function firstDayWeek(week, year) {
     }
     year = parseInt(year);
 
-   // Special case for year 1000
-   if (year === 1000 && week === 1) {
-    return '01-01-1000';
-}
+    if (year === 1000) {
+        const firstDayOfYear = new Date(1000, 0, 1);
+        const daysToAdd = (week - 1) * 7;
+        const resultDate = new Date(firstDayOfYear.getTime() + daysToAdd * 24 * 60 * 60 * 1000);
+
+        let dd = String(resultDate.getDate()).padStart(2, '0');
+        let mm = String(resultDate.getMonth() + 1).padStart(2, '0');
+        let yyyy = resultDate.getFullYear();
+        
+        return `${dd}-${mm}-${yyyy}`;
+    }
 
     let firstDay = new Date(year, 0, 1);
 
