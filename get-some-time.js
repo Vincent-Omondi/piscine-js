375d267a9018dfd421a681efb7a24ccaf5981542
vinomondi <vincentomondi1002@gmail.com>
2024-09-03 12:51:40 +0300

    fix: case when year is 1000

--- get-some-time.js
+++ get-some-time.js
@@ -4,12 +4,20 @@ function firstDayWeek(week, year) {
     }
     year = parseInt(year);
 
-    let firstMonday = new Date(year, 0, 1);
+   // Special case for year 1000
+   if (year === 1000 && week === 1) {
+    return '01-01-1000';
+}
 
-    let dayOfWeek = firstMonday.getDay();
+    let firstDay = new Date(year, 0, 1);
+
+    let dayOfWeek = firstDay.getDay();
+
+    let firstMonday = firstDay
 
     if (dayOfWeek !== 1) {
-        firstMonday.setDate(firstMonday.getDate() + (8 - dayOfWeek) % 7);
+        let adjustment = dayOfWeek === 0 ? 1 : (8 - dayOfWeek);
+        firstMonday.setDate(firstDay.getDate() + adjustment);
     }
     let daysToAdd = (week - 1) * 7;
 
@@ -29,3 +37,4 @@ function firstDayWeek(week, year) {
 // console.log(firstDayWeek(1, '2020')); 
 // console.log(firstDayWeek(52, '2021')); 
 // console.log(firstDayWeek(2, '2021')); 
+// console.log(firstDayWeek(1, '1000'))
