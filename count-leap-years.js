4626bfc098707c10178d6ab763e93271407e3ebe
vinomondi <vincentomondi1002@gmail.com>
2024-09-03 15:21:25 +0300

    fix: leapyear count

--- count-leap-years.js
+++ count-leap-years.js
@@ -2,7 +2,7 @@ function countLeapYears(date) {
     let year = date.getFullYear();
     let leapYears = 0;
   
-    for (let i = 1; i <= year; i++) {
+    for (let i = 1; i < year; i++) {
       if (isLeapYear(i)) {
         leapYears++;
       }
@@ -17,3 +17,4 @@ function isLeapYear(year) {
 
 // console.log(countLeapYears(new Date('2024-01-01')));
 // console.log(countLeapYears(new Date('2023-01-01'))); 
+// console.log(countLeapYears(new Date('1664-08-09')))
