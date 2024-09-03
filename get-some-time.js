380ff286b737f858bc23a7adad1d832aad9cc227
vinomondi <vincentomondi1002@gmail.com>
2024-09-03 14:00:21 +0300

    fix:

--- get-some-time.js
+++ get-some-time.js
@@ -1,35 +1,35 @@
 function firstDayWeek(week, year) {
-    if (week < 1 || week > 53 || isNaN(year)) {
-        return;
+    let time = new Date(year);
+    if (week === 1) {
+        time.setHours(24);
+        return formattedDate(time);
     }
-    year = parseInt(year);
-
-    let firstDay = new Date(year, 0, 1);
-
-    let dayOfWeek = firstDay.getDay();
-
-    let firstMonday = firstDay
-
-    if (dayOfWeek !== 1) {
-        let adjustment = dayOfWeek === 0 ? 1 : (8 - dayOfWeek);
-        firstMonday.setDate(firstDay.getDate() + adjustment);
-    }
-    let daysToAdd = (week - 1) * 7;
-
-    let resultDate = new Date(firstMonday.getTime() + daysToAdd * 24 * 60 * 60 * 1000);
-
-    if (resultDate.getFullYear() < year) {
-        resultDate = new Date(year, 0, 1);
-    }
-
-    let dd = String(resultDate.getDate()).padStart(2, '0');
-    let mm = String(resultDate.getMonth() + 1).padStart(2, '0');
-    let yyyy = resultDate.getFullYear();
     
-    return `${dd}-${mm}-${yyyy}`;
+    let dayPlus = week * 7 * 24;
+    time.setHours(dayPlus - 123);
+    
+    for (let i = 0; i < 7; i++) {
+        if (getWeekDay(time) === 'Monday') {
+            return formattedDate(time);
+        }
+        time.setHours(-24);
+    }
+    return time;
 }
 
-// console.log(firstDayWeek(1, '2020')); 
-console.log(firstDayWeek(1, '2025')); 
-console.log(firstDayWeek(52, '1000')); 
-console.log(firstDayWeek(1, '1000'))
+function getWeekDay(date) {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return days[date.getDay() - 1];
+}
+
+function formattedDate(d) {
+    let month = String(d.getMonth() + 1).padStart(2, '0');
+    let day = String(d.getDate() - 1).padStart(2, '0');
+    let year = String(d.getFullYear()).padStart(4, '0');
+    return `${day}-${month}-${year}`;
+}
+
+
+// console.log(firstDayWeek(1, '2025')); 
+// console.log(firstDayWeek(52, '1000')); 
+// console.log(firstDayWeek(1, '1000'))
