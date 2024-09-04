7f8d4b87d05681116ed13efc78bae9f54f1ee331
vinomondi <vincentomondi1002@gmail.com>
2024-09-04 14:32:07 +0300

    feat: Implement flatMap function

---
+++ mapper.js
@@ -0,0 +1,23 @@
+const map = (arr, fn) => {
+    const result = [];
+    for (let i = 0; i < arr.length; i++){
+        result.push(fn(arr[i]));
+    }
+    return result;
+};
+
+const flatMap = (arr, fn) => {
+    // return map(arr, fn).reduce((acc, val) => acc.concat(val), []);
+    const result = [];
+    for (let i = 0; i < arr.length; i++){
+        const val = fn(arr[i]);
+        if(Array.isArray(val)){
+            result.push(...val);
+        } else {
+            result.push(val);
+        }
+    }
+    return result;
+};
+console.log(flatMap([1, 2, 3], x => [x, x * 2]));
+console.log(map([1, 2, 3], x => x * 2));
