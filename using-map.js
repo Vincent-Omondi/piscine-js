f89d2c2473f99c9ec9860a75313783c7b30f7987
vinomondi <vincentomondi1002@gmail.com>
2024-09-04 10:43:15 +0300

    fix: Standardize temperature formatting in output strings

--- using-map.js
+++ using-map.js
@@ -35,7 +35,7 @@ function tempForecasts(arrObj){
                 .join(' ')
                 ;
 
-        return `${celsius}°Celsius in ${str.city},${capitalizedcity}`
+        return `${celsius}°Celsius in ${str.city}, ${capitalizedcity}`
     });
 }
 
