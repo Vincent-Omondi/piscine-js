3dda5eabd4cef118f67bc4ed4335b0656488508b
vinomondi <vincentomondi1002@gmail.com>
2024-09-05 09:37:51 +0300

    fix: refactor the code so user can define cities array on ftheir choice

--- has-city.js
+++ has-city.js
@@ -1,16 +1,10 @@
-const hasCity = (country, city) => {
-    return () => cities.includes(city)? `${city} is a city from ${country}` :
+const hasCity = (country, cities) => {
+    return (city) => cities.includes(city)? `${city} is a city from ${country}` :
          `${city} is not a city from ${country}`;
 }
 
-// const cities = ['New York', 'London', 'Tokyo', 'Sydney'];
+// const isKenyaCity = hasCity('Kenya', ['Kisumu', 'Nairobi', 'Mombasa']);
 
+// console.log(isKenyaCity('Kisumu'));    
+// console.log(isKenyaCity('London'));    
 
-// const isNewYorkCity = hasCity('USA', 'New York');
-// console.log(isNewYorkCity()); 
-// const isLondonCity = hasCity('UK', 'London');
-// console.log(isLondonCity()); 
-// const isTokyoCity = hasCity('Japan', 'Tokyo');
-// console.log(isTokyoCity()); 
-// const isNairobiCity = hasCity('Australia', 'Nairobi');
-// console.log(isNairobiCity()); 
