55aac09129142f6c2fab42836db6d4b10a9adbe8
vinomondi <vincentomondi1002@gmail.com>
2024-09-09 15:03:37 +0300

    Implement `fusion` function to merge objects based on value types
    - Concatenate arrays from both objects.
    - Concatenate strings with a space.
    - Add numbers from both objects.
    - Recursively merge nested objects.
    - Replace values in case of type mismatches with values from the second object if they exist.
    - Handle additional properties from the second object that are not in the first.

---
+++ fusion.js
@@ -0,0 +1,28 @@
+const fusion = (obj1, obj2) => {
+    const result = {};
+
+    for (const key in obj1) {
+        if (Array.isArray(obj1[key])) {
+            result[key] = obj1[key].concat(obj2[key] || []);
+        } else if (typeof obj1[key] ==='string' && typeof obj2[key] === 'string') {
+            result[key] = obj1[key] +' ' + obj2[key];
+        } else if (typeof obj1[key] === 'number' && typeof obj2[key] === 'number') {
+            result[key] = obj1[key] + obj2[key];
+        } else if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+            result[key] = fusion(obj1[key], obj2[key]);
+        } else {
+            result[key] = obj2[key] || obj1[key];
+        }
+    }
+
+    for (const key in obj2) {
+        if (!(key in obj1)) {
+            result[key] = obj2[key];
+        }
+    }
+
+    return result;
+}
+
+
+// console.log(fusion({ a: "hello", b: [] }, { a: 4 }))
