301022a0e87629b3eabfaaa01d96c9b7bdec5034
vinomondi <vincentomondi1002@gmail.com>
2024-09-12 15:47:57 +0300

    fix: opThrottle

--- throttle.js
+++ throttle.js
@@ -9,36 +9,27 @@ const throttle = (func, wait) => {
     };
 }
 
-function opThrottle(func, wait, options = {}) {
-    let timeout = null;
-    let previous = 0;
-    let result;
-  
-    return function throttled(...args) {
-      const now = Date.now();
-      const later = () => {
-        previous = options.leading === false ? 0 : Date.now();
-        timeout = null;
-        result = func.apply(this, args);
-      };
-  
-      if (!previous && options.leading === false) {
-        previous = now;
-      }
-  
-      const remaining = wait - (now - previous);
-  
-      if (remaining <= 0 || remaining > wait) {
-        if (timeout) {
-          clearTimeout(timeout);
-          timeout = null;
+function opThrottle(fn, delay, { leading = false, trailing = true } = {}) {
+    let last = 0;
+    let timer = null;
+    return function () {
+        const now = +new Date();
+        if (!last && leading === false) {
+            last = now;
         }
-        previous = now;
-        result = func.apply(this, args);
-      } else if (!timeout && options.trailing !== false) {
-        timeout = setTimeout(later, remaining);
-      }
-  
-      return result;
+        if (now - last > delay) {
+            if (timer) {
+                clearTimeout(timer);
+                timer = null;
+            }
+            fn.apply(this, arguments);
+            last = now;
+        } else if (!timer && trailing !== false) {
+            timer = setTimeout(() => {
+                fn.apply(this, arguments);
+                last = +new Date();
+                timer = null;
+            }, delay);
+        }
     };
-  }
+}
