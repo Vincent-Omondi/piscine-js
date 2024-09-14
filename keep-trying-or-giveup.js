0c722ae9ffebcd417de1d726b919c8173c5cbe43
Vincent-Omondi <vincentomondi251@gmail.com>
2024-09-14 09:01:44 +0300

    fix: the number of retries

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -1,55 +1,28 @@
-function retry(count, callback) {
-  return async function(...args) {
-    let attempts = 0;
-    while (true) {
-      try {
-        return await callback(...args);
-      } catch (error) {
-        attempts++;
-        if (attempts > count) {
-          throw new Error('Max retries reached');
+function retry(count = 3, callback = async () => {}) {
+    return async function (...args) {
+        try {
+            return await callback(...args);
+        } catch (error) {
+            if (count > 0) {
+                return retry(count - 1, callback)(...args);
+            } else {
+                throw error;
+            }
         }
-      }
-    }
-  };
+    };
 }
 
-function timeout(delay, callback) {
-  return async function(...args) {
-    const timeoutPromise = new Promise((_, reject) => {
-      setTimeout(() => reject(new Error('timeout')), delay);
-    });
-
-    const callbackPromise = callback(...args);
-
-    return Promise.race([callbackPromise, timeoutPromise]);
-  };
+function timeout(delay = 0, callback = async () => {}) {
+    return async function (...args) {
+        const timeoutPromise = new Promise((_, reject) =>
+            setTimeout(() => reject(new Error('timeout')), delay)
+        );
+        const callbackPromise = callback(...args);
+        
+        try {
+            return await Promise.race([timeoutPromise, callbackPromise]);
+        } catch (error) {
+            throw error;
+        }
+    };
 }
-
-// Example usage of retry
-// const unreliableFunction = async () => {
-//   const random = Math.random();
-//   if (random < 0.8) {
-//     throw new Error('Random failure');
-//   }
-//   return 'Success!';
-// };
-
-// const retryFunction = retry(3, unreliableFunction);
-
-// retryFunction()
-//   .then(result => console.log('Retry result:', result))
-//   .catch(error => console.error('Retry error:', error.message));
-
-// // Example usage of timeout
-// const slowFunction = async () => {
-//   await new Promise(resolve => setTimeout(resolve, 2000));
-//   return 'Slow operation complete';
-// };
-
-// const timeoutFunction = timeout(1500, slowFunction);
-
-// timeoutFunction()
-//   .then(result => console.log('Timeout result:', result))
-//   .catch(error => console.error('Timeout error:', error.message));
-
