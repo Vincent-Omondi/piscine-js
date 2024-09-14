77ab0ceab9d5bd99555a524da0a655e969281cf2
Vincent-Omondi <vincentomondi251@gmail.com>
2024-09-14 11:39:39 +0300

    fix: enforce strict 80ms timeout in gougleSearc

--- gougle-search.js
+++ gougle-search.js
@@ -3,54 +3,33 @@
 
 // queryServers function
 async function queryServers(serverName, q) {
-    const mainUrl = `/${serverName}?q=${q}`;
+    const url = `/${serverName}?q=${q}`;
     const backupUrl = `/${serverName}_backup?q=${q}`;
-
-    // Race between the main and backup server to get the fastest response
-    return Promise.race([getJSON(mainUrl), getJSON(backupUrl)]);
+    
+    return Promise.race([getJSON(url), getJSON(backupUrl)]);
 }
 
-// Timeout function to reject after 80ms
-function withTimeout(promise, ms) {
-    return new Promise((resolve, reject) => {
-        const timeoutId = setTimeout(() => reject(new Error('timeout')), ms);
-        promise
-            .then((res) => {
-                clearTimeout(timeoutId);
-                resolve(res);
-            })
-            .catch((err) => {
-                clearTimeout(timeoutId);
-                reject(err);
-            });
-    });
-}
-
-// gougleSearch function
 async function gougleSearch(q) {
+    const timeoutPromise = new Promise((_, reject) =>
+        setTimeout(() => reject(new Error('timeout')), 80)
+    );
+
+    const webPromise = queryServers('web', q);
+    const imagePromise = queryServers('image', q);
+    const videoPromise = queryServers('video', q);
+
     try {
-        // Set up the three server queries
-        const webQuery = queryServers('web', q);
-        const imageQuery = queryServers('image', q);
-        const videoQuery = queryServers('video', q);
-
-        // Run all queries concurrently with a timeout of 80ms
-        const results = await Promise.race([
-            Promise.all([webQuery, imageQuery, videoQuery]),
-            new Promise((_, reject) => setTimeout(() => reject(new Error('timeout')), 80))
+        const [web, image, video] = await Promise.race([
+            timeoutPromise, 
+            Promise.all([webPromise, imagePromise, videoPromise])
         ]);
-
-        // Return the results in an object with server names as keys
-        return {
-            web: results[0],
-            image: results[1],
-            video: results[2],
-        };
+        
+        return { web, image, video };
     } catch (error) {
-        // Return error if timeout occurs
-        return error;
+        throw error;
     }
 }
+
 
 
 // gougleSearch('hello+world')
