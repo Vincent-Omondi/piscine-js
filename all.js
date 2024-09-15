bd1c2703358b4b0569b0f3a73ee3c40fdb779d97
Vincent-Omondi <vincentomondi251@gmail.com>
2024-09-15 08:05:28 +0300

    implement 'all' function that works like promise.all

---
+++ all.js
@@ -0,0 +1,10 @@
+const all = async (objs = {}) => {
+  if (Object.keys(objs).length === 0) return {};
+
+  const entries = Object.entries(objs);
+  const resolvedEntries = await Promise.all(
+    entries.map(async ([key, value]) => [key, await value])
+  );
+
+  return Object.fromEntries(resolvedEntries);
+};
