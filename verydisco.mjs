daf6411c7a9e97374f2c84ba076f8db168ce186a
vinomondi <vincentomondi1002@gmail.com>
2024-09-17 10:12:25 +0300

    implement node script that takes each argument passes in the dentence and apply "very disco"

---
+++ verydisco.mjs
@@ -0,0 +1,17 @@
+#!/usr/bin/env node 
+
+function makeVeryDisco(word) {
+    const midpoint = Math.ceil(word.length / 2);
+    return word.slice(midpoint) + word.slice(0, midpoint)
+}
+
+const input = process.argv.slice(2).join(' ');
+
+if (!input) {
+    console.error('Please provide a word as an argument.');
+    process.exit(1);
+}
+
+const veryDiscoResult = input.split(' ').map(makeVeryDisco).join(' ');
+
+console.log(veryDiscoResult)
